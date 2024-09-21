8344949a683c605e1f62441d5b8aff87e89ec18d
Arman <ovakimian.al@phystech.edu>
2024-09-21 21:31:14 +0300

    upd

--- contest1/contest1J.cpp
+++ contest1/contest1J.cpp
@@ -5,35 +5,32 @@
 #include <string>
 #include <vector>
 
-const int InvalidIndex = -10;
+const int InvalidIndex = -2;
 
-struct LcsInfo
-{
-  int lcs;
-  std::vector< std::pair<size_t, size_t> > lcsIndexes;
-};
-
-struct IthLcsInfo
-{
+struct IthLcsInfo {
   int lcs;
   std::pair<int, int> prevIndex;
 
-  IthLcsInfo() { lcs = 0; prevIndex = { InvalidIndex, InvalidIndex }; }
+  IthLcsInfo() {
+    lcs = 0;
+    prevIndex = {InvalidIndex, InvalidIndex};
+  }
 };
 
-std::vector<std::pair<size_t, size_t> > getLCSIndexes(
-    const std::vector< std::vector<IthLcsInfo> >& lcs,
-    int firstWordBeginPos, int secondWordBeginPos) {
-  std::vector<std::pair<size_t, size_t> > indexes;
+std::vector<std::pair<size_t, size_t>> getLCSIndexes(
+    const std::vector<std::vector<IthLcsInfo>>& lcs, int firstWordBeginPos,
+    int secondWordBeginPos) {
+  std::vector<std::pair<size_t, size_t>> indexes;
+  indexes.reserve(lcs[firstWordBeginPos][secondWordBeginPos].lcs);
   
   std::pair<int, int> indexesNow = {firstWordBeginPos, secondWordBeginPos};
-  while (indexesNow.first >= 0 && indexesNow.second >= 0)
-  {
-    std::pair<int, int> prevIndexes = lcs[indexesNow.first][indexesNow.second].prevIndex;
-    
-    if (prevIndexes.first  == indexesNow.first - 1 && 
+  while (indexesNow.first >= 0 && indexesNow.second >= 0) {
+    std::pair<int, int> prevIndexes =
+        lcs[indexesNow.first][indexesNow.second].prevIndex;
+
+    if (prevIndexes.first == indexesNow.first - 1 &&
         prevIndexes.second == indexesNow.second - 1)
-        indexes.push_back({indexesNow.first, indexesNow.second});
+      indexes.push_back({indexesNow.first, indexesNow.second});
 
     indexesNow = prevIndexes;
   }
@@ -43,11 +40,13 @@ std::vector<std::pair<size_t, size_t> > getLCSIndexes(
   return indexes;
 }
 
-LcsInfo getLcs(const std::string& firstWord, const std::string& secondWord) {
+std::vector<std::pair<size_t, size_t>> getLcs(const std::string& firstWord,
+                                              const std::string& secondWord) {
   size_t firstWordSize = firstWord.size();
   size_t secondWordSize = secondWord.size();
 
-  std::vector< std::vector<IthLcsInfo> > lcs(firstWordSize, std::vector<IthLcsInfo>(secondWordSize));
+  std::vector<std::vector<IthLcsInfo>> lcs(
+      firstWordSize, std::vector<IthLcsInfo>(secondWordSize));
 
   for (size_t i = 0; i < firstWordSize; ++i) {
     if (firstWord[i] == secondWord[0]) {
@@ -69,32 +68,29 @@ LcsInfo getLcs(const std::string& firstWord, const std::string& secondWord) {
         lcs[i][j].prevIndex = {i - 1, j - 1};
       } else {
         if (lcs[i - 1][j].lcs > lcs[i][j - 1].lcs) {
-          lcs[i][j] = lcs[i - 1][j];
+          lcs[i][j].lcs = lcs[i - 1][j].lcs;
+          lcs[i][j].prevIndex = {i - 1, j};
         } else {
-          lcs[i][j] = lcs[i][j - 1];
+          lcs[i][j].lcs = lcs[i][j - 1].lcs;
+          lcs[i][j].prevIndex = {i, j - 1};
         }
       }
     }
   }
 
-  LcsInfo result;
-  result.lcs = lcs[firstWordSize - 1][secondWordSize - 1].lcs;
-  result.lcsIndexes = getLCSIndexes(lcs, firstWordSize - 1, secondWordSize - 1);
-
-  return result;
+  return getLCSIndexes(lcs, firstWordSize - 1, secondWordSize - 1);
 }
 
-void printAnswer(const LcsInfo& lcs)
-{
-  std::cout << lcs.lcs << "\n";
+void printAnswer(const std::vector<std::pair<size_t, size_t>>& lcsIndexes) {
+  std::cout << lcsIndexes.size() << "\n";
 
-  for (size_t i = 0; i < lcs.lcsIndexes.size(); ++i)
-    std::cout << lcs.lcsIndexes[i].first + 1 << " ";
+  for (size_t i = 0; i < lcsIndexes.size(); ++i)
+    std::cout << lcsIndexes[i].first + 1 << " ";
 
   std::cout << "\n";
 
-  for (size_t i = 0; i < lcs.lcsIndexes.size(); ++i)
-    std::cout << lcs.lcsIndexes[i].second + 1 << " ";
+  for (size_t i = 0; i < lcsIndexes.size(); ++i)
+    std::cout << lcsIndexes[i].second + 1 << " ";
 }
 
 int main() {
@@ -102,7 +98,7 @@ int main() {
 
   std::cin >> firstWord >> secondWord;
 
-  LcsInfo lcs = getLcs(firstWord, secondWord);
+  auto lcsIndexes = getLcs(firstWord, secondWord);
 
-  printAnswer(lcs);
+  printAnswer(lcsIndexes);
 }
