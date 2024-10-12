6385fb6bb74bd79603ac2dbd22bea726e31224e6
Arman <ovakimian.al@phystech.edu>
2024-10-12 17:49:20 +0300

    contest1I

--- contest1/contest1I.cpp
+++ contest1/contest1I.cpp
@@ -10,17 +10,6 @@ struct MaxAreaInfo {
   int maxNumberOfAreasIndex = 0;
 };
 
-struct AreasInfo {
-  MaxAreaInfo maxArea;
-  std::vector<int> prevAreas;
-
-  AreasInfo(MaxAreaInfo maxAreaInit, const std::vector<int>& prevs)
-      : maxArea(maxAreaInit), prevAreas(prevs) {}
-
-  AreasInfo(MaxAreaInfo maxAreaInit, const std::vector<int>&& prevs)
-      : maxArea(maxAreaInit), prevAreas(prevs) {}
-};
-
 struct IthAreaInfo {
   int areasGot;
   int prevArea;
@@ -28,22 +17,11 @@ struct IthAreaInfo {
   IthAreaInfo() : areasGot(1), prevArea(-1) {}
 };
 
-inline std::vector<int> convertDpToPrevs(
-    const std::vector<IthAreaInfo>& areas) {
-  size_t areasSize = areas.size();
-  std::vector<int> prevs(areasSize);
-  for (size_t i = 0; i < areasSize; ++i) {
-    prevs[i] = areas[i].prevArea;
-  }
-
-  return prevs;
-}
-
-MaxAreaInfo findMaxArea(const std::vector<IthAreaInfo>& areas) {
+MaxAreaInfo findMaxArea(const std::vector<IthAreaInfo>& areasDP) {
   MaxAreaInfo maxArea;
-  for (size_t i = 0; i < areas.size(); ++i) {
-    if (maxArea.maxNumberOfAreas < areas[i].areasGot) {
-      maxArea.maxNumberOfAreas = areas[i].areasGot;
+  for (size_t i = 0; i < areasDP.size(); ++i) {
+    if (maxArea.maxNumberOfAreas < areasDP[i].areasGot) {
+      maxArea.maxNumberOfAreas = areasDP[i].areasGot;
       maxArea.maxNumberOfAreasIndex = i;
     }
   }
@@ -51,77 +29,96 @@ MaxAreaInfo findMaxArea(const std::vector<IthAreaInfo>& areas) {
   return maxArea;
 }
 
-AreasInfo findAreasPrevs(const std::vector<int>& a,
-                         bool sequenceCondition(int)) {
-  std::vector<IthAreaInfo> areas(a.size());
+std::vector<int> restoreAreasToCaptureBasedOnAreasDP(
+    const std::vector<int>& numberOfGangstersInAreas,
+    const std::vector<IthAreaInfo>& areasDP) {
+  MaxAreaInfo maxArea = findMaxArea(areasDP);
 
-  for (size_t i = 1; i < a.size(); ++i) {
+  int i = maxArea.maxNumberOfAreasIndex;
+  std::vector<int> areasToCapture;
+  while (i != -1) {
+    areasToCapture.push_back(numberOfGangstersInAreas[i]);
+    i = areasDP[i].prevArea;
+  }
+
+  std::reverse(areasToCapture.begin(), areasToCapture.end());
+
+  return areasToCapture;
+}
+
+std::vector<IthAreaInfo> calculateAreasDP(
+    const std::vector<int>& numberOfGangstersInAreas,
+    bool sequenceCondition(int)) {
+  std::vector<IthAreaInfo> areasDP(numberOfGangstersInAreas.size());
+
+  for (size_t i = 1; i < numberOfGangstersInAreas.size(); ++i) {
     for (size_t j = 0; j < i; ++j) {
-      if (areas[i].areasGot < areas[j].areasGot + 1) {
-        if (sequenceCondition(areas[j].areasGot) && a[j] > a[i]) {
-          areas[i].areasGot = areas[j].areasGot + 1;
-          areas[i].prevArea = j;
-        } else if (!sequenceCondition(areas[j].areasGot) && a[j] < a[i]) {
-          areas[i].areasGot = areas[j].areasGot + 1;
-          areas[i].prevArea = j;
+      if (areasDP[i].areasGot < areasDP[j].areasGot + 1) {
+        if (sequenceCondition(areasDP[j].areasGot) &&
+            numberOfGangstersInAreas[j] > numberOfGangstersInAreas[i]) {
+          areasDP[i].areasGot = areasDP[j].areasGot + 1;
+          areasDP[i].prevArea = j;
+        } else if (!sequenceCondition(areasDP[j].areasGot) &&
+                   numberOfGangstersInAreas[j] < numberOfGangstersInAreas[i]) {
+          areasDP[i].areasGot = areasDP[j].areasGot + 1;
+          areasDP[i].prevArea = j;
         }
       }
     }
   }
 
-  return AreasInfo(findMaxArea(areas), convertDpToPrevs(areas));
+  return areasDP;
 }
 
-std::vector<int> findBestAreasIndexes(const std::vector<int>& a,
-                                      const AreasInfo& areasInfo) {
-  int i = areasInfo.maxArea.maxNumberOfAreasIndex;
-  std::vector<int> ans;
-  while (i != -1) {
-    ans.push_back(a[i]);
-    i = areasInfo.prevAreas[i];
-  }
-
-  std::reverse(ans.begin(), ans.end());
-
-  return ans;
+std::vector<int> findAreasToCaptureWithSequenceConditionStrategy(
+    const std::vector<int>& numberOfGangstersInAreas,
+    bool sequenceCondition(int)) {
+  return restoreAreasToCaptureBasedOnAreasDP(
+      numberOfGangstersInAreas,
+      calculateAreasDP(numberOfGangstersInAreas, sequenceCondition));
 }
 
 bool isEven(int x) { return x % 2 == 0; }
 bool isOdd(int x) { return x % 2 == 1; }
 
-AreasInfo findBestAreas(const std::vector<int>& a) {
-  AreasInfo areaInfo1 = findAreasPrevs(a, isEven);
-  AreasInfo areaInfo2 = findAreasPrevs(a, isOdd);
+std::vector<int> findAreasToCapture(
+    const std::vector<int>& numberOfGangstersInAreas) {
+  auto areasToCaptureStrategy1 =
+      findAreasToCaptureWithSequenceConditionStrategy(numberOfGangstersInAreas,
+                                                      isEven);
+  auto areasToCaptureStrategy2 =
+      findAreasToCaptureWithSequenceConditionStrategy(numberOfGangstersInAreas,
+                                                      isOdd);
 
-  if (areaInfo1.maxArea.maxNumberOfAreas < areaInfo2.maxArea.maxNumberOfAreas) {
-    return areaInfo2;
+  if (areasToCaptureStrategy1.size() < areasToCaptureStrategy2.size()) {
+    return areasToCaptureStrategy2;
   }
 
-  return areaInfo1;
-}
-
-std::vector<int> findBestAreasIndexes(const std::vector<int>& a) {
-  return findBestAreasIndexes(a, findBestAreas(a));
+  return areasToCaptureStrategy1;
 }
 
 std::vector<int> readInput() {
-  size_t n = 0;
-  std::cin >> n;
+  size_t nAreas = 0;
+  std::cin >> nAreas;
 
-  std::vector<int> a(n);
-  std::copy_n(std::istream_iterator<int>(std::cin), n, a.begin());
-  return a;
+  std::vector<int> numberOfGangstersInAreas(nAreas);
+  std::copy_n(std::istream_iterator<int>(std::cin), nAreas,
+              numberOfGangstersInAreas.begin());
+  return numberOfGangstersInAreas;
 }
 
-void printBestAreas(const std::vector<int>& ans) {
-  std::cout << ans.size() << "\n";
-  std::copy(ans.begin(), ans.end(), std::ostream_iterator<int>(std::cout, " "));
+void printAreasToCapture(const std::vector<int>& areasToCapture) {
+  std::cout << areasToCapture.size() << "\n";
+
+  std::copy(areasToCapture.begin(), areasToCapture.end(),
+            std::ostream_iterator<int>(std::cout, " "));
 }
 
 int main() {
-  std::vector<int> a = readInput();
+  std::vector<int> numberOfGangstersInAreas = readInput();
 
-  std::vector<int> ans = findBestAreasIndexes(a);
+  std::vector<int> areasToCapture =
+      findAreasToCapture(numberOfGangstersInAreas);
 
-  printBestAreas(ans);
+  printAreasToCapture(areasToCapture);
 }
