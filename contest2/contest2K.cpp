0c8b277ee5fd5243ff621a11268ea568f0a9b0e5
Arman <ovakimian.al@phystech.edu>
2024-10-14 00:17:35 +0300

    google cs

--- contest2/contest2K.cpp
+++ contest2/contest2K.cpp
@@ -1,30 +1,28 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
 #include <cassert>
+#include <iostream>
+#include <vector>
 
-struct Point
-{
+struct Point {
   int x, y;
 
   explicit Point(int x = 0, int y = 0) : x(x), y(y) {}
 };
 
-struct Line
-{
+struct Line {
   int slopeFactor, yOffset;
 
-  explicit Line(int slopeFactor = 0, int yOffset = 0) : slopeFactor(slopeFactor), yOffset(yOffset) {}
+  explicit Line(int slopeFactor = 0, int yOffset = 0)
+      : slopeFactor(slopeFactor), yOffset(yOffset) {}
 
   int getY(int x) const { return slopeFactor * x + yOffset; }
 };
 
-class LowerEnvelope
-{
+class LowerEnvelope {
   std::vector<Line> lines_;
-  std::vector<int>  linesIntersectionsX_;
+  std::vector<int> linesIntersectionsX_;
 
-public:
+ public:
   LowerEnvelope();
 
   void addLine(const Line& line);
@@ -32,8 +30,7 @@ public:
   int getY(int x) const;
 };
 
-int findCrossX(const Line& l1, const Line& l2)
-{
+int findCrossX(const Line& l1, const Line& l2) {
   assert(l1.slopeFactor != l2.slopeFactor);
 
   int x = (l2.yOffset - l1.yOffset) / (l1.slopeFactor - l2.slopeFactor);
@@ -42,20 +39,18 @@ int findCrossX(const Line& l1, const Line& l2)
   return x;
 }
 
-LowerEnvelope::LowerEnvelope() 
-{
+LowerEnvelope::LowerEnvelope() {
   static const int inf = 1e9;
 
   lines_.push_back(Line(0, inf));
   linesIntersectionsX_.push_back(-inf);
 }
 
-void LowerEnvelope::addLine(const Line& line)
-{
+void LowerEnvelope::addLine(const Line& line) {
   int intersectionX = linesIntersectionsX_.back();
 
-  while (lines_.size() != 0 && lines_.back().getY(intersectionX) > line.getY(intersectionX))
-  {
+  while (lines_.size() != 0 &&
+         lines_.back().getY(intersectionX) > line.getY(intersectionX)) {
     lines_.pop_back();
     linesIntersectionsX_.pop_back();
 
@@ -72,18 +67,16 @@ void LowerEnvelope::addLine(const Line& line)
   lines_.push_back(line);
 }
 
-int LowerEnvelope::getY(int x) const
-{
-  int lineIndex = std::lower_bound(linesIntersectionsX_.begin(), linesIntersectionsX_.end(), x) - 
+int LowerEnvelope::getY(int x) const {
+  int lineIndex = std::lower_bound(linesIntersectionsX_.begin(),
+                                   linesIntersectionsX_.end(), x) -
                   linesIntersectionsX_.begin() - 1;
 
   return lines_[lineIndex].getY(x);
 }
 
-int findMinSquareLens(const int n, const int k)
-{
-  if (k == 0)
-  {
+int findMinSquareLens(const int n, const int k) {
+  if (k == 0) {
     return 0;
   }
 
@@ -92,9 +85,8 @@ int findMinSquareLens(const int n, const int k)
   std::vector<std::vector<int> > dp(n + 1, std::vector<int>(k + 1, infDpVal));
 
   LowerEnvelope envelope;
-  
-  for (int i = 0; i <= k; ++i)
-  {
+
+  for (int i = 0; i <= k; ++i) {
     dp[0][i] = 0;
   }
 
@@ -102,17 +94,16 @@ int findMinSquareLens(const int n, const int k)
 
   envelope.addLine(zeroPointsLine);
 
-  for (int numberOfSegments = 1; numberOfSegments <= k; ++numberOfSegments)
-  {
+  for (int numberOfSegments = 1; numberOfSegments <= k; ++numberOfSegments) {
     LowerEnvelope newEnvelope;
     newEnvelope.addLine(zeroPointsLine);
 
-    for (int pointId = 1; pointId <= n; ++pointId)
-    {
-      dp[pointId][numberOfSegments] = envelope.getY(pointId) + pointId * pointId;
+    for (int pointId = 1; pointId <= n; ++pointId) {
+      dp[pointId][numberOfSegments] =
+          envelope.getY(pointId) + pointId * pointId;
       newEnvelope.addLine(
-        Line{-2 * (pointId + 1), dp[pointId][numberOfSegments] + (pointId + 1) * (pointId + 1)}
-      );
+          Line{-2 * (pointId + 1),
+               dp[pointId][numberOfSegments] + (pointId + 1) * (pointId + 1)});
     }
 
     envelope = newEnvelope;
@@ -121,8 +112,7 @@ int findMinSquareLens(const int n, const int k)
   return dp[n][k];
 }
 
-int main()
-{
+int main() {
   int n = 0, k = 0;
   std::cin >> n >> k;
 
