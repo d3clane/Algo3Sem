67fe0beb3001d49deb6e07ccb99fd38bae37c576
Arman <ovakimian.al@phystech.edu>
2024-12-15 19:17:29 +0300

    class graph

--- contest5/contest5F.cpp
+++ contest5/contest5F.cpp
@@ -2,32 +2,48 @@
 #include <iostream>
 #include <vector>
 
-std::vector<std::vector<bool>> readGraphAdjMatrix() {
+class Graph {
+ public:
+  Graph() = default;
+  explicit Graph(std::vector<std::vector<bool>> graphAdjMatrix)
+      : adjacentMatrix_(graphAdjMatrix) {}
+
+  Graph findTransitiveClosure();
+
+  void printGraphAdjacentMatrix();
+
+  static Graph readGraphFromAdjacentMatrix();
+
+ private:
+  std::vector<std::vector<bool>> adjacentMatrix_;
+};
+
+Graph Graph::readGraphFromAdjacentMatrix() {
   size_t n;
   std::cin >> n;
 
-  std::vector<std::vector<bool>> graphAdjMatrix(n, std::vector<bool>(n, false));
+  std::vector<std::vector<bool>> graphAdjacentMatrix(
+      n, std::vector<bool>(n, false));
 
   for (size_t i = 0; i < n; i++) {
     for (size_t j = 0; j < n; j++) {
       int value = 0;
       std::cin >> value;
       if (value == 0) {
-        graphAdjMatrix[i][j] = 0;
+        graphAdjacentMatrix[i][j] = 0;
       } else {
-        graphAdjMatrix[i][j] = 1;
+        graphAdjacentMatrix[i][j] = 1;
       }
     }
   }
 
-  return graphAdjMatrix;
+  return Graph{graphAdjacentMatrix};
 }
 
-std::vector<std::vector<bool>> findTransitiveClosure(
-    const std::vector<std::vector<bool>>& graphAdjMatrix) {
-  size_t n = graphAdjMatrix.size();
+Graph Graph::findTransitiveClosure() {
+  size_t n = adjacentMatrix_.size();
 
-  std::vector<std::vector<bool>> closure = graphAdjMatrix;
+  std::vector<std::vector<bool>> closure = adjacentMatrix_;
 
   for (size_t k = 0; k < n; k++) {
     for (size_t i = 0; i < n; i++) {
@@ -37,20 +53,21 @@ std::vector<std::vector<bool>> findTransitiveClosure(
     }
   }
 
-  return closure;
+  return Graph{closure};
 }
 
-void printGraphAdjMatrix(const std::vector<std::vector<bool>>& graphAdjMatrix) {
-  for (size_t i = 0; i < graphAdjMatrix.size(); i++) {
-    for (size_t j = 0; j < graphAdjMatrix.size(); j++) {
-      std::cout << (int)graphAdjMatrix[i][j] << " ";
+void Graph::printGraphAdjacentMatrix() {
+  for (size_t i = 0; i < adjacentMatrix_.size(); i++) {
+    for (size_t j = 0; j < adjacentMatrix_[i].size(); j++) {
+      std::cout << static_cast<int>(adjacentMatrix_[i][j]) << " ";
     }
     std::cout << std::endl;
   }
 }
 
 int main() {
-  auto graphAdjMatrix = readGraphAdjMatrix();
-  auto closure = findTransitiveClosure(graphAdjMatrix);
-  printGraphAdjMatrix(closure);
+  Graph graph = Graph::readGraphFromAdjacentMatrix();
+  Graph closure = graph.findTransitiveClosure();
+
+  closure.printGraphAdjacentMatrix();
 }
